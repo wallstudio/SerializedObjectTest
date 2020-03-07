afe37946428c0997aa90e794c341d03442de33ef
UpHash <yukawallstudio@gmail.com>
2020-03-07 15:57:49 +0900

    SerializedObjectTraversaer

---
+++ Assets/SerializedObjectTraversaer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+using System.Linq;
+
+public class SerializedObjectTraversaer
+{
+    [MenuItem("Assets/" + nameof(SerializedObjectTraversaer) + "/" + nameof(SerializedObjectTraversaer.Dump))]
+    public static void Dump()
+    {
+        var log = new StringBuilder();
+
+        var target = Selection.activeObject;
+        var serialized = new SerializedObject(target);
+        var iterator = serialized.GetIterator();
+        while(iterator.Next(true))
+        {
+            log.AppendLine($"{iterator.name}:");
+            var props = iterator.GetType().GetProperties();
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(iterator);
+                log.AppendLine($"  {prop.Name:30}: {value}");
+            }
+        }
+
+        GUIUtility.systemCopyBuffer = log.ToString();
+        Debug.Log(log);
+    }
+}
